18d8d7376d1dfd409f65a148c9bbd36761a9434e
bonniesu <32457847+bonniesu@users.noreply.github.com>
2018-09-18 15:42:25 -0400

    Update MainActivity.java

--- app/src/main/java/taplytics/newqaapp/MainActivity.java
+++ app/src/main/java/taplytics/newqaapp/MainActivity.java
@@ -260,7 +260,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         }
     }
 
-    public void onClickActivity(final View view) {
-        SecondActivity.startActivity(this);
+//     public void onClickActivity(final View view) {
+//         SecondActivity.startActivity(this);
     }
 }
